a647f9d29966a2a0500e5fa1cf714f8b64be00cd
sangil <tkddlf59@gmail.com>
2019-08-06 08:42:58 +0900

    Add Query Builder

---
+++ pkg/rsdb/query.go
@@ -0,0 +1,69 @@
+package rsdb
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/pkg/errors"
+
+	"github.com/realsangil/apimonitor/pkg/rserrors"
+	"github.com/realsangil/apimonitor/pkg/rsvalid"
+)
+
+type queryOperator string
+
+const (
+	AndQueryOperator queryOperator = "AND"
+	OrQueryOperator  queryOperator = "OR"
+)
+
+type QueryBuilder interface {
+	Or(q Query) Query
+	And(q Query) Query
+}
+
+type Query interface {
+	QueryBuilder
+	Where() string
+	Values() []interface{}
+}
+
+type queryImpl struct {
+	where  string
+	values []interface{}
+}
+
+func (query *queryImpl) combineQueryWithOperator(q Query, operator queryOperator) Query {
+	query.where = fmt.Sprintf("(%s) %s (%s)", query.Where(), operator, q.Where())
+	query.values = append(query.values, q.Values()...)
+	return query
+}
+
+func (query *queryImpl) Or(q Query) Query {
+	return query.combineQueryWithOperator(q, OrQueryOperator)
+}
+
+func (query *queryImpl) And(q Query) Query {
+	return query.combineQueryWithOperator(q, AndQueryOperator)
+}
+
+func (query *queryImpl) Where() string {
+	return query.where
+}
+
+func (query *queryImpl) Values() []interface{} {
+	return query.values
+}
+
+func NewQuery(w string, values ...interface{}) (Query, error) {
+	if rsvalid.IsZero(w) {
+		return nil, errors.Wrap(rserrors.ErrInvalidParameter, "invalid wheres")
+	}
+	if strings.Count(w, "?") != len(values) {
+		return nil, errors.Wrap(rserrors.ErrInvalidParameter, "invalid values")
+	}
+	return &queryImpl{
+		where:  w,
+		values: values,
+	}, nil
+}
