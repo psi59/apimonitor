f3000352298a2ab536d64a3183651d8ef9bf89aa
sangil <tkddlf59@gmail.com>
2019-09-04 09:49:38 +0900

    Add http package

---
+++ pkg/http/http.go
@@ -0,0 +1,99 @@
+package http
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/pkg/errors"
+	"github.com/realsangil/apimonitor/pkg/rserrors"
+)
+
+const (
+	MethodGet     = "GET"
+	MethodHead    = "HEAD"
+	MethodPost    = "POST"
+	MethodPut     = "PUT"
+	MethodPatch   = "PATCH"
+	MethodDelete  = "DELETE"
+	MethodConnect = "CONNECT"
+	MethodOptions = "OPTIONS"
+	MethodTrace   = "TRACE"
+
+	MIMEApplicationJSON       = "application/json"
+	MIMEApplicationJavaScript = "application/javascript"
+	MIMEApplicationXML        = "application/xml"
+	MIMETextXML               = "text/xml"
+	MIMEApplicationForm       = "application/x-www-form-urlencoded"
+	MIMEApplicationProtobuf   = "application/protobuf"
+	MIMEApplicationMsgpack    = "application/msgpack"
+	MIMETextHTML              = "text/html"
+	MIMETextPlain             = "text/plain"
+	MIMEMultipartForm         = "multipart/form-data"
+	MIMEOctetStream           = "application/octet-stream"
+)
+
+var (
+	MIMEApplicationJSONCharsetUTF8       = appendUTF8IntoMIME(MIMEApplicationJSON)
+	MIMEApplicationJavaScriptCharsetUTF8 = appendUTF8IntoMIME(MIMEApplicationJavaScript)
+	MIMEApplicationXMLCharsetUTF8        = appendUTF8IntoMIME(MIMEApplicationXML)
+	MIMETextXMLCharsetUTF8               = appendUTF8IntoMIME(MIMETextXML)
+	MIMETextHTMLCharsetUTF8              = appendUTF8IntoMIME(MIMETextHTML)
+	MIMETextPlainCharsetUTF8             = appendUTF8IntoMIME(MIMETextPlain)
+
+	appendUTF8IntoMIME = func(mime string) string {
+		return fmt.Sprintf("%s; charset=UTF-8", mime)
+	}
+)
+
+var regexpEndpointPath = regexp.MustCompile(`^[\/a-zA-Z0-9_.\-~!$&'()*+,;=:@]+$`)
+
+type EndpointPath string
+
+func (endpointPath EndpointPath) String() string {
+	return string(endpointPath)
+}
+
+func (endpointPath EndpointPath) Validate() error {
+	if regexpEndpointPath.MatchString(endpointPath.String()) {
+		return nil
+	}
+	return errors.Wrap(rserrors.ErrInvalidParameter, "EndpointPath")
+}
+
+type Method string
+
+func (method Method) String() string {
+	return string(method)
+}
+
+func (method Method) Validate() error {
+	switch method {
+	case MethodGet:
+	case MethodHead:
+	case MethodPost:
+	case MethodPut:
+	case MethodPatch:
+	case MethodDelete:
+	case MethodConnect:
+	case MethodOptions:
+	case MethodTrace:
+	default:
+		return errors.Wrap(rserrors.ErrInvalidParameter, "Method")
+	}
+	return nil
+}
+
+var regexpContentType = regexp.MustCompile(`(text|application|multipart)/(javascript|json|x-www-form-urlencoded|octet-stream|form-data|xml)(;(.+))?`)
+
+type ContentType string
+
+func (contentType ContentType) String() string {
+	return string(contentType)
+}
+
+func (contentType ContentType) Validate() error {
+	if regexpContentType.MatchString(contentType.String()) {
+		return nil
+	}
+	return errors.Wrap(rserrors.ErrInvalidParameter, "ContentType")
+}
---
+++ pkg/http/http_test.go
@@ -0,0 +1,211 @@
+package http
+
+import (
+	"testing"
+)
+
+func TestEndpointPath_String(t *testing.T) {
+	tests := []struct {
+		name         string
+		endpointPath EndpointPath
+		want         string
+	}{
+		{
+			name:         "pass",
+			endpointPath: "/test",
+			want:         "/test",
+		},
+		{
+			name:         "pass",
+			endpointPath: "/test/path",
+			want:         "/test/path",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.endpointPath.String(); got != tt.want {
+				t.Errorf("String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestEndpointPath_Validate(t *testing.T) {
+	tests := []struct {
+		name         string
+		endpointPath EndpointPath
+		wantErr      bool
+	}{
+		{
+			name:         "pass",
+			endpointPath: "/test",
+			wantErr:      false,
+		},
+		{
+			name:         "invalid path",
+			endpointPath: "??/?",
+			wantErr:      true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.endpointPath.Validate(); (err != nil) != tt.wantErr {
+				t.Errorf("Validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestHttpMethod_Validate(t *testing.T) {
+	tests := []struct {
+		name    string
+		method  Method
+		wantErr bool
+	}{
+		{
+			name:    "get",
+			method:  MethodGet,
+			wantErr: false,
+		},
+		{
+			name:    "head",
+			method:  MethodHead,
+			wantErr: false,
+		},
+		{
+			name:    "post",
+			method:  MethodPost,
+			wantErr: false,
+		},
+		{
+			name:    "put",
+			method:  MethodPut,
+			wantErr: false,
+		},
+		{
+			name:    "patch",
+			method:  MethodPatch,
+			wantErr: false,
+		},
+		{
+			name:    "delete",
+			method:  MethodDelete,
+			wantErr: false,
+		},
+		{
+			name:    "connect",
+			method:  MethodConnect,
+			wantErr: false,
+		},
+		{
+			name:    "options",
+			method:  MethodOptions,
+			wantErr: false,
+		},
+		{
+			name:    "trace",
+			method:  MethodTrace,
+			wantErr: false,
+		},
+		{
+			name:    "invalid",
+			method:  "invalid",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.method.Validate(); (err != nil) != tt.wantErr {
+				t.Errorf("Validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestMethod_String(t *testing.T) {
+	tests := []struct {
+		name   string
+		method Method
+		want   string
+	}{
+		{
+			name:   "pass",
+			method: "GET",
+			want:   "GET",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.method.String(); got != tt.want {
+				t.Errorf("String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestContentType_String(t *testing.T) {
+	tests := []struct {
+		name        string
+		contentType ContentType
+		want        string
+	}{
+		{
+			name:        "pass",
+			contentType: "text/json",
+			want:        "text/json",
+		},
+		{
+			name:        "pass",
+			contentType: "application/xml",
+			want:        "application/xml",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.contentType.String(); got != tt.want {
+				t.Errorf("String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestContentType_Validate(t *testing.T) {
+	tests := []struct {
+		name        string
+		contentType ContentType
+		wantErr     bool
+	}{
+		{
+			name:        "pass",
+			contentType: "application/xml",
+			wantErr:     false,
+		},
+		{
+			name:        "pass",
+			contentType: "application/xml; utf-8",
+			wantErr:     false,
+		},
+		{
+			name:        "pass",
+			contentType: "text/javascript",
+			wantErr:     false,
+		},
+		{
+			name:        "pass",
+			contentType: "multipart/form-data",
+			wantErr:     false,
+		},
+		{
+			name:        "invalid contentType",
+			contentType: "application",
+			wantErr:     true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.contentType.Validate(); (err != nil) != tt.wantErr {
+				t.Errorf("Validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
